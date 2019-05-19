87522811cf220fef2da37fd6e51670680870e802
mkgask <mmp@zsw.jp>
2019-05-19 19:36:50 +0900

    [add] base npm & vue-cli config

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    outputDir: 'docs',
+    assetsDir: 'assets',
+
+    pages: {
+        index: {
+            entry: 'src/index/index.vue',
+            title: 'zsw.jp - @mkgask web site.',
+        }
+    }
+}
