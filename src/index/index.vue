8a8273fe58fade593377ec5fb572e9fc90d1387f
mkgask <mmp@zsw.jp>
2019-05-19 19:39:16 +0900

    [add] input/output files

---
+++ src/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        zsw.jp
+    </div>
+</template>
+
+<style lang="sass">
+#app
+    color: #111
+</style>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    el: '#app'
+})
+</script>
