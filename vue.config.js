6c68d2caa161ffbe437bfe0c59a57446510d9bca
mkgask <mmp@zsw.jp>
2019-05-21 01:32:15 +0900

    [add] enable page title

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,14 @@
 module.exports = {
     outputDir: 'docs',
     assetsDir: 'assets',
+    publicPath: 'assets',
 
     pages: {
         index: {
             entry: 'src/index/index.vue',
+            template: 'src/index.template.html',
             title: 'zsw.jp - @mkgask web site.',
-            chunks: ['main-nav']
+            chunks: ['chunk-vendors', 'chunk-common', 'main-nav']
         }
     }
 }
