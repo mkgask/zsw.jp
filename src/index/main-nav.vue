4e68c12e24dc9bbac0a4ad519e6aa53f9fff951a
mkgask <mmp@zsw.jp>
2019-06-12 00:59:27 +0900

    [add] class change when selected

--- src/index/main-nav.vue
+++ src/index/main-nav.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
     div#MainNav
-        v-btn.mainnav_zswjp(href="#Top",ripple=true,block=true) zsw.jp
-        v-btn.mainnav_results(href="#Results",ripple=true,block=true) Results
-        v-btn.mainnav_game(href="#Game",color="error",ripple=true,block=true) Game
-        v-btn.mainnav_movie(href="#Movie",color="warning",ripple=true,block=true) Movie
-        v-btn.mainnav_github(href="#GitHub",color="black",ripple=true,block=true,dark) GitHub
-        v-btn.mainnav_qiita(href="#Qiita",color="success",ripple=true,block=true) Qiita
-        v-btn.mainnav_twitter(href="#Twitter",color="info",ripple=true,block=true) Twitter
+        v-btn.mainnav_zswjp(href="#Top",ripple=true,block=true,:class="[selected == 'top' ? 'selected' : '']",@click="select('top')") zsw.jp
+        v-btn.mainnav_results(href="#Results",ripple=true,block=true,:class="[selected == 'results' ? 'selected' : '']",@click="select('results')") Results
+        v-btn.mainnav_game(href="#Game",color="error",ripple=true,block=true,:class="[selected == 'game' ? 'selected' : '']",@click="select('game')") Game
+        v-btn.mainnav_movie(href="#Movie",color="warning",ripple=true,block=true,:class="[selected == 'movie' ? 'selected' : '']",@click="select('movie')") Movie
+        v-btn.mainnav_github(href="#GitHub",color="black",ripple=true,block=true,dark,:class="[selected == 'github' ? 'selected' : '']",@click="select('github')") GitHub
+        v-btn.mainnav_qiita(href="#Qiita",color="success",ripple=true,block=true,:class="[selected == 'qiita' ? 'selected' : '']",@click="select('qiita')") Qiita
+        v-btn.mainnav_twitter(href="#Twitter",color="info",ripple=true,block=true,:class="[selected == 'twitter' ? 'selected' : '']",@click="select('twitter')") Twitter
 </template>
 
 <style scoped lang="sass">
@@ -23,11 +23,51 @@
 
 #MainNav a.mainnav_zswjp
     text-transform: lowercase
+
+#MainNav a.mainnav_zswjp.selected
     border-right: 10px #999 solid
+    border-right-color: #999
+
+#MainNav a.mainnav_results.selected
+    border-right: 10px #999 solid
+    border-right-color: #999
+
+#MainNav a.mainnav_game.selected
+    border-right: 10px #999 solid
+    border-right-color: #999
+
+#MainNav a.mainnav_movie.selected
+    border-right: 10px #999 solid
+    border-right-color: #999
+
+#MainNav a.mainnav_github.selected
+    border-right: 10px #999 solid
+    border-right-color: #999
+
+#MainNav a.mainnav_qiita.selected
+    border-right: 10px #999 solid
+    border-right-color: #999
+
+#MainNav a.mainnav_twitter.selected
+    border-right: 10px #999 solid
+    border-right-color: #999
 </style>
 
 <script lang="ts">
 import Vue from 'vue';
 
-export default {}
+export default {
+
+    data: function() {
+        return {
+            selected: ''
+        }
+    },
+
+    methods: {
+        select: function(s = '') {
+            this.selected = s
+        }
+    }
+}
 </script>
