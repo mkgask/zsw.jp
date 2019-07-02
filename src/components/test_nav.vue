84ed95430720e7c8b7b47c25295cb27be6abce90
mkgask <mmp@zsw.jp>
2019-07-03 00:45:41 +0900

    [update] change stylesheet type sass to stylus

--- src/components/test_nav.vue
+++ src/components/test_nav.vue
@@ -5,26 +5,31 @@
             :class="class_latest"
             @click="select('latest')"
         ) Latest
+
         v-btn.nav_game(
             ripple=true, block=true,
             :class="class_game"
             @click="select('game')"
         ) Game
+
         v-btn.nav_movie(
             ripple=true, block=true,
             :class="class_movie"
             @click="select('movie')"
         ) Movie
+
         v-btn.nav_app(
             ripple=true, block=true,
             :class="class_app"
             @click="select('app')"
         ) App
+
         v-btn.nav_link(
             ripple=true, block=true,
             :class="class_link"
             @click="select('link')"
         ) Link
+
         v-btn.nav_menu(
             ripple=true, block=true,
             class="default"
@@ -32,43 +37,78 @@
         ) Menu
 </template>
 
-<style scoped lang="sass">
+<style scoped lang="stylus">
+
+contents = {
+    latest: {
+        default: {
+            right: 0
+        }
+    }
+
+    game: {
+        default: {
+            right: -5%
+        }
+    }
+
+    movie: {
+        default: {
+            right: -10%
+        }
+    }
+
+    app: {
+        default: {
+            right: -15%
+        }
+    }
+
+    link: {
+        default: {
+            right: -20%
+        }
+    }
+
+    menu: {
+        default: {
+            right: -25%
+        }
+    }
+
+}
+
+for key, content in contents
+    for key2, styles in content
+        #nav .nav_{key}.{key2}
+            right: styles.right
+
 #nav
     font-family: "Kosugi Maru"
 
 #nav button
+    position: fixed
+    z-index: 1000
+    bottom: 5%
     text-transform: capitalize
     width: 100vh
     max-width: 100vw
     transform: rotate(-75deg)
     padding: 1.2em 0
     font-size: 1.6em
-
 </style>
 
-<style scoped lang="sass" src="./styles/nav_latest.sass">
-</style>
-
-<style scoped lang="sass" src="./styles/nav_game.sass">
-</style>
-
-<style scoped lang="sass" src="./styles/nav_movie.sass">
-</style>
-
-<style scoped lang="sass" src="./styles/nav_app.sass">
-</style>
-
-<style scoped lang="sass" src="./styles/nav_link.sass">
-</style>
-
-<style scoped lang="sass" src="./styles/nav_menu.sass">
-</style>
+<style scoped lang="stylus" src="./styles/nav_latest.styl"></style>
+<style scoped lang="stylus" src="./styles/nav_game.styl"></style>
+<style scoped lang="stylus" src="./styles/nav_movie.styl"></style>
+<style scoped lang="stylus" src="./styles/nav_app.styl"></style>
+<style scoped lang="stylus" src="./styles/nav_link.styl"></style>
+<style scoped lang="stylus" src="./styles/nav_menu.styl"></style>
 
 <script lang="ts">
 import Vue from 'vue';
 
 export default {
-
     data: function() {
         return {
             selected: 'menu'
@@ -81,24 +121,25 @@ export default {
             if (this.selected == 'latest') return 'active'
             return 'deactive'
         },
-        
+
         class_game: function() {
             if (this.selected == 'menu') return 'default'
             if (this.selected == 'game') return 'active'
             return 'deactive'
         },
-        
+
         class_movie: function() {
             if (this.selected == 'menu') return 'default'
             if (this.selected == 'movie') return 'active'
             return 'deactive'
         },
+
         class_app: function() {
             if (this.selected == 'menu') return 'default'
             if (this.selected == 'app') return 'active'
             return 'deactive'
         },
-        
+
         class_link: function() {
             if (this.selected == 'menu') return 'default'
             if (this.selected == 'link') return 'active'
