85b8be12c80b2e3c780ae33385de5104f914ab51
mkgask <mmp@zsw.jp>
2019-10-20 00:49:57 +0900

    [update] update smartphone style in link component

--- src/pages/link.vue
+++ src/pages/link.vue
@@ -11,9 +11,11 @@
                 a.content_link(
                     v-show="content.type != 'niconico'"
                     :href="content.url",
-                    :style="{ backgroundImage: 'url(' + content.image + ')' }"
                 )
-                    span.content_text
+                    span.content_image_box
+                        img.content_image(:src="content.image")
+
+                    span.content_text_box
                         span.content_title {{content.title}}
                         span.content_body {{content.body}}
 
@@ -41,72 +43,15 @@
 export default {
     data: function () {
         return {
-            list: [
-                {
-                    type: 'qiita',
-                    url: 'https://qiita.com/mkgask',
-                    image: require('@/images/qiita-square.png'),
-                    src: '',
-                    title: 'Qiita',
-                    body: 'Unity, C#, Python, Kivy, Docker, Laravelあたりのことを書いています。',
-                    firsttime: '2015/09/16',
-                    updatetime: '2018/10/29'
-                },
-                {
-                    type: 'github',
-                    url: 'https://github.com/mkgask',
-                    image: require('@/images/GitHub_Logo.png'),
-                    src: '',
-                    title: 'GitHub',
-                    body: '色々雑多に、書いたものを置いています。',
-                    firsttime: '2015/10/22',
-                    updatetime: '2019/08/28'
-                },
-                {
-                    type: 'unityroom',
-                    url: 'https://unityroom.com/users/mkgask',
-                    image: require('@/images/UniDark/Thumnail2.jpg'),
-                    src: '',
-                    title: 'unityroom',
-                    body: 'Unityで製作したWebGLのゲームを置いています。無料公開はだいたいここに上げていく予定です。',
-                    firsttime: '2019/03/09',
-                    updatetime: '2019/03/10'
-                },
-                {
-                    type: 'niconico',
-                    url: 'https://www.nicovideo.jp/user/519329/video',
-                    image: '',
-                    src: 'https://ext.nicovideo.jp/thumb_user/519329',
-                    title: 'ニコニコ動画',
-                    body: '',
-                    firsttime: '2007/09/09',
-                    updatetime: '2019/04/25'
-                },
-                {
-                    type: 'twitter',
-                    url: 'https://twitter.com/mkgask',
-                    image: require('@/images/Twitter_Logo_Blue.png'),
-                    src: '',
-                    title: 'Twitter',
-                    body: '技術的なことも日常のことも区別せずに色々呟いています。全ログは https://twilog.org/mkgask にあります。',
-                    firsttime: '2009/9/12',
-                    updatetime: 'ほぼ毎日'
-                },
-                {
-                    type: 'pixiv',
-                    url: 'https://www.pixiv.net/member.php?id=477235',
-                    image: require('@/images/pixiv.png'),
-                    src: '',
-                    title: 'Pixiv',
-                    body: '見せられないよ',
-                    firsttime: '2018/06/03',
-                    updatetime: '2018/06/09'
-                }
-            ],
+            list: [],
 
             show_num: 10,
             per_page: 10
         }
+    },
+
+    mounted: function () {
+        this.$data.list = this.$store.getters.get_link_list
     },
 
     methods: {
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -7,7 +7,7 @@ export default new Vuex.Store({
     state: {
         route: 'menu',
 
-        list: [
+        latest_list: [
             {
                 type: 'github',
                 url: 'https://github.com/mkgask/zsw.jp',
@@ -428,6 +428,69 @@ export default new Vuex.Store({
                 firsttime: '2007/09/09',
                 updatetime: ''
             }
+        ],
+
+        link_list: [
+            {
+                type: 'qiita',
+                url: 'https://qiita.com/mkgask',
+                image: require('@/images/qiita-square.png'),
+                src: '',
+                title: 'Qiita',
+                body: 'Unity, C#, Python, Kivy, Docker, Laravelあたりのことを書いています。',
+                firsttime: '2015/09/16',
+                updatetime: '2018/10/29'
+            },
+            {
+                type: 'github',
+                url: 'https://github.com/mkgask',
+                image: require('@/images/GitHub_Logo.png'),
+                src: '',
+                title: 'GitHub',
+                body: '色々雑多に、書いたものを置いています。',
+                firsttime: '2015/10/22',
+                updatetime: '2019/08/28'
+            },
+            {
+                type: 'unityroom',
+                url: 'https://unityroom.com/users/mkgask',
+                image: require('@/images/UniDark/Thumnail2.jpg'),
+                src: '',
+                title: 'unityroom',
+                body: 'Unityで製作したWebGLのゲームを置いています。無料公開はだいたいここに上げていく予定です。',
+                firsttime: '2019/03/09',
+                updatetime: '2019/03/10'
+            },
+            {
+                type: 'niconico',
+                url: 'https://www.nicovideo.jp/user/519329/video',
+                image: '',
+                src: 'https://ext.nicovideo.jp/thumb_user/519329',
+                title: 'ニコニコ動画',
+                body: '',
+                firsttime: '2007/09/09',
+                updatetime: '2019/04/25'
+            },
+            {
+                type: 'twitter',
+                url: 'https://twitter.com/mkgask',
+                image: require('@/images/Twitter_Logo_Blue.png'),
+                src: '',
+                title: 'Twitter',
+                body: '技術的なことも日常のことも区別せずに色々呟いています。全ログは https://twilog.org/mkgask にあります。',
+                firsttime: '2009/9/12',
+                updatetime: 'ほぼ毎日'
+            },
+            {
+                type: 'pixiv',
+                url: 'https://www.pixiv.net/member.php?id=477235',
+                image: require('@/images/pixiv.png'),
+                src: '',
+                title: 'Pixiv',
+                body: '見せられないよ',
+                firsttime: '2018/06/03',
+                updatetime: '2018/06/09'
+            }
         ]
     },
 
@@ -437,11 +500,15 @@ export default new Vuex.Store({
         },
 
         get_latest_list: state => {
-            return state.list
+            return state.latest_list
         },
 
         get_movie_list: state => {
-            return state.list.filter((value) => value.type === 'niconico')
+            return state.latest_list.filter((value) => value.type === 'niconico')
+        },
+
+        get_link_list: state => {
+            return state.link_list
         }
     },
 
