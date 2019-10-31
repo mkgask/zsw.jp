992080f3b50f28560c32d520b42cc9411c8021d4
mkgask <mmp@zsw.jp>
2019-10-31 23:07:02 +0900

    [update] allow camelcase to variable name in esint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,22 @@
 module.exports = {
     root: true,
+
     env: {
         node: true
     },
+
     'extends': [
         'plugin:vue/essential',
         '@vue/standard'
     ],
+
     rules: {
         'indent': ['error', 4],
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'camelcase': 0
     },
+
     parserOptions: {
         parser: 'babel-eslint'
     }
